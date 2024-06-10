daa3409aa24956db2f2e2fc6843e7adf478267ed
Deepanshu Thakur <Deepanshu.Thakur@Geminisolutions.com>
2024-06-10 23:50:18 +0530

    Login backend frontend implmented

--- App/LoginScreen.js
+++ App/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -12,49 +12,43 @@ import {
   Platform,
   TouchableWithoutFeedback,
   Keyboard,
-} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import athena from '../assets/images/Athen_Logo.png'
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import athena from "../assets/images/Athen_Logo.png";
 
 export default function LoginScreen() {
   const navigation = useNavigation();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [fdata, setFdata] = useState({
+    email: "",
+    password: "",
+  });
 
-  const validateEmail = (email) => {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(String(email).toLowerCase());
+  const [errormsg, setErrormsg] = useState(null);
+
+  const handleLogin = async() => {
+    // console.log(fdata);
+    if (fdata.email == "" || fdata.password == "") {
+      setErrormsg("All fields are required");
+      return;
+    }
+      try {
+        console.log(fdata);
+        const response = await axios.post('http://192.168.4.153:3000/signin', fdata);
+        
+        if (response.data.error) {
+          setErrormsg(response.data.error);
+        } else {
+          alert('Account created successfully');
+          navigation.navigate('Success');
+        }
+      } catch (error) {
+        setErrormsg('Error connecting to the server');
+      }
+
   };
-
-  const handleLogin = () => {
-    let valid = true;
-
-    if (!email || !validateEmail(email)) {
-      setEmailError('Please enter a valid email.');
-      valid = false;
-    } else {
-      setEmailError('');
-    }
-
-    if (!password) {
-      setPasswordError('Please enter a password.');
-      valid = false;
-    } else {
-      setPasswordError('');
-    }
-
-    if (valid) {
-      console.log('Email:', email);
-      console.log('Password:', password);
-      navigation.navigate('Success');
-    }
-  };
-
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.container}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -65,25 +59,29 @@ export default function LoginScreen() {
           <View>
             <Text style={styles.emailtext}>Email*</Text>
             <TextInput
-              style={[styles.input, emailError ? styles.errorInput : null]}
-              onChangeText={setEmail}
+              style={styles.input}
+              onChangeText={(text)=>setFdata({...fdata,email:text})}
               placeholder="Enter Email"
-              value={email}
+              // value={email}
               keyboardType="email-address"
               autoCapitalize="none"
             />
-            {emailError ? <Text style={styles.errorText}>{emailError}</Text> : null}
+            {/* {errormsg ? (
+              <Text style={styles.errorText}>{errormsg}</Text>
+            ) : null} */}
           </View>
           <View>
             <Text style={styles.emailtext}>Password*</Text>
             <TextInput
-              style={[styles.input, passwordError ? styles.errorInput : null]}
-              onChangeText={setPassword}
+              style={styles.input}
+              onChangeText={(text)=>setFdata({...fdata,password: text})}
               placeholder="Enter Password"
-              value={password}
+              // value={password}/
               secureTextEntry
             />
-            {passwordError ? <Text style={styles.errorText}>{passwordError}</Text> : null}
+            {/* {errormsg ? (
+              <Text style={styles.errorText}>{errormsg}</Text>
+            ) : null} */}
           </View>
           <View style={styles.forgot}>
             <TouchableOpacity>
@@ -91,10 +89,18 @@ export default function LoginScreen() {
             </TouchableOpacity>
           </View>
           <View style={styles.signin}>
-            <Button title="Sign in" color='#0f4c75' onPress={handleLogin}></Button>
+            <Button
+              title="Sign in"
+              color="#0f4c75"
+              onPress={handleLogin}
+            ></Button>
           </View>
           <View style={styles.logingem}>
-            <Button title="Login with Gemini Id" color='#3e885b' onPress={() => navigation.navigate('Signup')}></Button>
+            <Button
+              title="Login with Gemini Id"
+              color="#3e885b"
+              onPress={() => navigation.navigate("Signup")}
+            ></Button>
           </View>
         </SafeAreaView>
       </TouchableWithoutFeedback>
@@ -108,13 +114,13 @@ const styles = StyleSheet.create({
   },
   inner: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   image: {
     width: 250,
     height: 250,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginTop: 20,
   },
   input: {
@@ -123,24 +129,24 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginTop: 10,
     padding: 10,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
     borderRadius: 10,
   },
   errorInput: {
-    borderColor: 'red',
+    borderColor: "red",
   },
   emailtext: {
-    color: '#808080',
-    alignItems: 'flex-start',
+    color: "#808080",
+    alignItems: "flex-start",
     marginTop: 13,
   },
   forgot: {
     marginTop: 20,
-    alignItems: 'flex-end',
+    alignItems: "flex-end",
   },
   forgottext: {
-    color: '#0f4c75',
-    alignItems: 'flex-end',
+    color: "#0f4c75",
+    alignItems: "flex-end",
     marginStart: 140,
   },
   signin: {
@@ -148,12 +154,12 @@ const styles = StyleSheet.create({
     width: 250,
   },
   logingem: {
-    color: 'green',
+    color: "green",
     marginTop: 30,
     width: 250,
   },
   errorText: {
-    color: 'red',
+    color: "red",
     fontSize: 12,
   },
 });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -32,72 +32,74 @@ router.post('/signup', async (req, res) => {
 })
 
 
-// router.post('/verify', (req, res) => {
-//     console.log('sent by client - ', req.body);
-//     const { name, email, password, dob, address } = req.body;
-//     if (!name || !email || !password || !dob || !address) {
-//         return res.status(422).json({ error: "Please add all the fields" });
-//     }
+router.post('/verify', (req, res) => {
+    console.log('sent by client - ', req.body);
+    const { name, email, password, dob, address } = req.body;
+    if (!name || !email || !password || !dob || !address) {
+        return res.status(422).json({ error: "Please add all the fields" });
+    }
 
 
-//     User.findOne({ email: email })
-//         .then(async (savedUser) => {
-//             if (savedUser) {
-//                 return res.status(422).json({ error: "Invalid Credentials" });
-//             }
-//             try {
+    User.findOne({ email: email })
+        .then(async (savedUser) => {
+            if (savedUser) {
+                return res.status(422).json({ error: "Invalid Credentials" });
+            }
+            try {
 
-//                 let VerificationCode = Math.floor(100000 + Math.random() * 900000);
-//                 let user = [
-//                     {
-//                         name,
-//                         email,
-//                         password,
-//                         dob,
-//                         address,
-//                         VerificationCode
-//                     }
-//                 ]
-//                 await mailer(email, VerificationCode);
-//                 res.send({ message: "Verification Code Sent to your Email", udata: user });
-//             }
-//             catch (err) {
-//                 console.log(err);
-//             }
-//         })
-
-
-// })
+                let VerificationCode = Math.floor(100000 + Math.random() * 900000);
+                let user = [
+                    {
+                        name,
+                        email,
+                        password,
+                        dob,
+                        address,
+                        VerificationCode
+                    }
+                ]
+                await mailer(email, VerificationCode);
+                res.send({ message: "Verification Code Sent to your Email", udata: user });
+            }
+            catch (err) {
+                console.log(err);
+            }
+        })
 
 
+})
 
-// router.post('/signin', async (req, res) => {
-//     const { email, password } = req.body;
-//     if (!email || !password) {
-//         return res.status(422).json({ error: "Please add email or password" });
-//     }
-//     const savedUser = await User.findOne({ email: email })
 
-//     if (!savedUser) {
-//         return res.status(422).json({ error: "Invalid Credentials" });
-//     }
 
-//     try {
-//         bcrypt.compare(password, savedUser.password, (err, result) => {
-//             if (result) {
-//                 console.log("Password matched");
-//                 const token = jwt.sign({ _id: savedUser._id }, process.env.JWT_SECRET);
-//                 res.send({ token });
-//             }
-//             else {
-//                 console.log('Password does not match');
-//                 return res.status(422).json({ error: "Invalid Credentials" });
-//             }
-//         })
-//     }
-//     catch (err) {
-//         console.log(err);
-//     }
-// })
+router.post('/signin', async (req, res) => {
+    console.log('sent by client - ', req.body);
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).json({ error: "Please add email or password" });
+    }
+    const savedUser = await User.findOne({ email: email })
+    console.log(savedUser);
+
+    if (!savedUser) {
+        return res.status(422).json({ error: "Invalid Credentials" });
+    }
+
+    try {
+        bcrypt.compare(password, savedUser.password, (err, result) => {
+            if (result) {
+                console.log("Password matched");
+                const token = jwt.sign({ _id: savedUser._id }, `${process.env.JWT_SECRET}`);
+                res.send({ token });
+            }
+            else {
+                console.log('Password does not match');
+                return res.status(422).json({ error: "Invalid Credentials" });
+            }
+        })
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
 
 module.exports = router;
