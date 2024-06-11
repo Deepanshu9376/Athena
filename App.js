716b8be08f11b33fd6514209462a0e7917e23ba5
Deepanshu Thakur <Deepanshu.Thakur@Geminisolutions.com>
2024-06-11 23:55:34 +0530

    Tab navigation

--- App.js
+++ App.js
@@ -2,12 +2,56 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from './App/LoginScreen';
 import SignupScreen from './App/SignupScreen';
-import SuccessScreen from './App/SuccessScreen';
+// import SuccessScreen from './App/SuccessScreen';
 import Verification from './App/Verification';
+import Home from './InnerScreen/Home';
+import Courses from './InnerScreen/Courses';
+import Profile from './InnerScreen/Profile';
+import Puzzles from './InnerScreen/Puzzles';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function BottomTabNavigator() {
+  return (
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({color,size}) => {
+        let iconName;
+
+        switch (route.name) {
+          case 'Home':
+            iconName = 'home-outline';
+            break;
+          case 'Courses':
+            iconName = 'book-outline';
+            break;
+          case 'Puzzles':
+            iconName = 'book-outline';
+            break;
+          case 'Profile':
+            iconName = 'person-outline';
+            break;
+        }
+
+        return <Icon name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: '#fff',
+      tabBarInactiveTintColor: '#bbbbbb',
+      tabBarStyle: { backgroundColor: '#0f4c75' },
+    })}
+  >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Courses" component={Courses} />
+      <Tab.Screen name="Puzzles" component={Puzzles} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -15,7 +59,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Success" component={SuccessScreen} />
+        <Stack.Screen name="Success" component={BottomTabNavigator} />
         <Stack.Screen name="Verification" component={Verification} />
       </Stack.Navigator>
     </NavigationContainer>
--- App/SuccessScreen.js
+++ App/SuccessScreen.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button,TouchableOpacity } from "react-native";
+
 
 export default function SuccessScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Login Successful!</Text>
-      <Button title="Go back" onPress={() => navigation.navigate('Login')} />
+      <View style={styles.mainContent}>
+        <Text style={styles.text}>Login Successful!</Text>
+        <Button title="Go back" onPress={() => navigation.navigate("Login")} />
+      </View>
     </View>
   );
 }
@@ -13,11 +16,30 @@ export default function SuccessScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  mainContent: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     fontSize: 24,
     marginBottom: 20,
+  },
+  taskbar: {
+    height: 60,
+    marginTop: 40,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderTopWidth: 1,
+    borderTopColor: "#e1e1e1",
+    backgroundColor: "#0f4c75",
+  },
+  taskbarButton: {
+    fontSize: 18,
+    color:'#fff'
   },
 });
